7d5d90b61182d03708904d0e5dbac1715aea43b9
Nestor Ivan Castellanos Laiton <n.nestel@hotmail.com>
2026-02-06 15:02:05 -0500

    feat: ajuste files

--- vars/staticAnalysis.groovy
+++ vars/staticAnalysis.groovy
@@ -1,8 +1,11 @@
 def call(Map params = [:]) {
+    // Parámetro para decidir si abortar siempre
     def abortPipeline = params.get('abortPipeline', false)
 
+    // Ejecutar análisis con SonarQube
     timeout(time: 5, unit: 'MINUTES') {
         withSonarQubeEnv('SonarQubeServer') {
+            // Ajusta las propiedades según tu proyecto real
             sh '''
                 sonar-scanner \
                 -Dsonar.projectKey=mi-proyecto \
@@ -13,10 +16,13 @@ def call(Map params = [:]) {
         }
     }
 
-    def qg = waitForQualityGate()
-    if (abortPipeline || qg.status != 'OK') {
-        error("Pipeline abortado: QualityGate = ${qg.status}")
-    } else {
-        echo "Pipeline continúa: QualityGate = ${qg.status}"
+    // Esperar resultados del Quality Gate con timeout
+    timeout(time: 5, unit: 'MINUTES') {
+        def qg = waitForQualityGate()
+        if (abortPipeline || qg.status != 'OK') {
+            error("Pipeline abortado: QualityGate = ${qg.status}")
+        } else {
+            echo "Pipeline continúa: QualityGate = ${qg.status}"
+        }
     }
 }
